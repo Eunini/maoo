f9f75b777519073ce90241bff1637094dd3df389
Helios AI <helios@ai.dev>
2026-02-28 19:31:46 +0100

    run automated script

--- demo.sh
+++ demo.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "start deployment..."
 PROJECT_NAME1=$3
-if [ -z "PROJECT_NAME1" ]; then
+if [ -z "$PROJECT_NAME1" ]; then
 echo "error, folder not found"
 echo "use: ./deploy.sh <project_name>"
 exit 1
 fi
-mkdir "PROJECT_NAME1"
+mkdir "$PROJECT_NAME1"
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
 echo "Error: failed to create to directory: $PROJECT_NAME1"
